33039aa69736d25ddcefd441e249d17340d54881
bysmartos <bysmartos@gmail.com>
2022-08-17 14:58:21 +0200

    first commit

---
+++ client/src/components/ExploreContainer.tsx
@@ -0,0 +1,12 @@
+import './ExploreContainer.css';
+import Login from "../components/Login";
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <Login/>
+  );
+};
+
+export default ExploreContainer;
