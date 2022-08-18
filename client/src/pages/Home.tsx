a75ab6941f3b1b71b4075e0bb21c291b6c9b2ee1
1ri5 <iriscyst@gmail.com>
2022-08-18 15:08:15 +0200

    #1 Home component imported

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,14 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import Blob from '../components/Blob';
-// import { SearchBar } from '../components/SearchBar';
+import { SearchBar } from '../components/SearchBar';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        {/* <SearchBar/> */}
+        <SearchBar/>
         <ExploreContainer />
         <Blob/>
       </IonContent>
