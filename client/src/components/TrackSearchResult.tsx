4ac0bc4c6b2872ac78cbf5f2c56302b1374fb858
1ri5 <iriscyst@gmail.com>
2022-08-21 11:45:28 +0200

    Track Search Result styled

--- client/src/components/TrackSearchResult.tsx
+++ client/src/components/TrackSearchResult.tsx
@@ -7,14 +7,13 @@ export default function TrackSearchResult({ track, chooseTrack }:any) {
 
   return (
     <div
-      
-      style={{ cursor: "pointer", display:"flex", alignItems: "center" }}
+    style={{ cursor: "pointer", display:"flex", alignItems: "center", marginTop: "1rem", marginLeft: "0.5rem", backgroundColor: "rgba(240, 240, 240, 0.4)", borderRadius: "50px"}}
       onClick={handlePlay}
     >
-      <img alt="img" src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
+      <img alt="img" src={track.albumUrl} style={{ height: "64px", width: "64px", borderRadius: "50px", marginRight: "0.5rem" }} />
       <div className="ml-3">
-        <div>{track.title}</div>
-        <div >{track.artist}</div>
+        <div style={{color: "#1e1e1e"}}>{track.title}</div>
+        <div style={{color: "#505050"}}>{track.artist}</div>
       </div>
     </div>
   )
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,13 +2,11 @@ import { IonContent, IonPage } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import Blob from '../components/Blob';
-import { SearchBar } from '../components/SearchBar';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <SearchBar/>
         <ExploreContainer />
         <Blob/>
       </IonContent>
