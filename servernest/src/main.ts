d6e1c3c55c4b779bbe801e6e6d8b19b1af3990c3
bysmartos <bysmartos@gmail.com>
2022-08-24 16:01:11 +0200

    whithout changing servernest route

--- servernest/src/main.ts
+++ servernest/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
